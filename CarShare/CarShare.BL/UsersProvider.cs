a7943cd10ebd7db4ee7c132da49c3654263bdb52
RomanSlyepko <roma.srt@gmail.com>
2016-11-24 23:23:49 +0200

    + UserController; UserProvider: addUser, getAllUsers

--- CarShare/CarShare.BL/UsersProvider.cs
+++ CarShare/CarShare.BL/UsersProvider.cs
@@ -14,5 +14,37 @@ namespace CarShare.BL
                 return db.Users.ToList();
             }
         }
+
+        public void AddUser(User user)
+        {
+            using (var db = new CarShareDBContext())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+        }
+        public void AddDefaultUser()
+        {
+            using (var db = new CarShareDBContext())
+            {
+                var us = new User();
+                us.FirstName = "Vasia";
+                us.LastName = "Surname";
+                
+                db.Users.Add(us);
+                db.SaveChanges();
+            }
+        }
+
+
+        public string Gesdfsd()
+        {
+            using (var db = new CarShareDBContext())
+            {
+                return "some return value";
+            }
+        }
+
+
     }
 }
